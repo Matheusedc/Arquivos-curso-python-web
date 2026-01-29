79689b15010314bc94fe6450ba2cb746210f247a
Matheusedc <matheuseddc@gmail.com>
2026-01-29 16:52:26 -0300

    Aula 12

--- fakesite/routes.py
+++ fakesite/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template, url_for, redirect
-from fakesite import app
-from flask_login import login_required
+from fakesite import app, database, bcrypt
+from flask_login import login_required, login_user, logout_user, current_user
+from fakesite.models import Usuario, Foto
 from fakesite.forms import FormLogin, FormCriarConta
 
 
@@ -8,22 +9,44 @@ from fakesite.forms import FormLogin, FormCriarConta
 
 def homepage():
     formlogin = FormLogin()
-    return render_template("homepage.html", form=formlogin)
-'''
-
-@app.route("/", methods=["GET", "POST"])
-
-def homepage():
-    formlogin = FormLogin()
+    
+    if formlogin.validate_on_submit():
+        usuario = Usuario.query.filter_by(email=email.data).first()
+        
+        if usuario and bcrypt.check_passord_hash(usuario.senha, form_login.senha.data):
+            
+            login_user(usuario, remember = True) #Loga o usuario e matém logado mesmo que feche a janela
+        
+            return  redirect(url_for("perfil", usuario = usuario.username))
+        
     return render_template("homepage.html", form=formlogin)
     
-@app.route("/criarconta")
+@app.route("/criarconta", methods=["GET", "POST"])
 def criarconta():
     formcriarconta = FormCriarConta()
+    if formcriarconta.validate_on_submit():
+        
+        senha = bcrypt.ganerate_password_hash()
+        
+        usuario = Usuario(username = form_criarconta.username.data , email = form_criarconta.email.data , senha = senha)
+        
+        database.session.add(usuario)
+        database.session.comit()
+
+        login_user(usuario, remember = True) #Loga o usuario e matém logado mesmo que feche a janela
+        
+        return redirect(url_for("perfil", usuario = usuario.username))
+        
     return render_template("criarconta.html", form=formcriarconta)
 
 @app.route("/perfil/<usuario>")
 @login_required
 def perfil(usuario):
     return render_template("perfil.html", usuario=usuario)
-'''
+
+@app.route("/logout")
+@login_required
+
+def logout():
+    logout_user()
+    return redirect(url_for("homepage", usuario = usuario.username))
