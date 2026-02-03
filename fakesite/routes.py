3410add9d83c7035b1c41d1beeb0d783a3a2a29e
Matheusedc <matheuseddc@gmail.com>
2026-02-03 12:56:57 -0300

    aula 15

--- fakesite/routes.py
+++ fakesite/routes.py
@@ -2,7 +2,10 @@ from flask import render_template, url_for, redirect
 from fakesite import app, database, bcrypt
 from flask_login import login_required, login_user, logout_user, current_user
 from fakesite.models import Usuario, Foto
-from fakesite.forms import FormLogin, FormCriarConta
+from fakesite.forms import FormLogin, FormCriarConta, FormFoto
+import os
+from werkzeug.utils import secure_filename
+
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -39,15 +42,30 @@ def criarconta():
         
     return render_template("criarconta.html", form=form_criarconta)
 
-@app.route("/perfil/<id_usuario>")
+@app.route("/perfil/<id_usuario>", methods=["GET", "POST"])
 @login_required
 def perfil(id_usuario):
     if int(id.usuario) == int(current_user.id):
         # o usuario ta vendo o perfil dele
-        return render_template("perfil.html", usuario=corrent_user)
+        form_foto = FormFoto()
+        
+        if form_foto.validate_on_submit():
+            arquivo = form.foto.foto.data
+            nome_seguro = secure_filename(arquivo.filename)
+            # salvar o arquivo na pasta fotos_post
+            caminho = os.parth.join(os.parth.abspath(os.path.dirname(__file__)), app.config["UPLOAD_FOLDER"], nome_seguro)
+            
+            arquivo.save(caminho)
+            
+            # registrar no banco de dados
+            foto = Foto(imagem = nome_seguro, id_usuario = current_user.id)
+            database.session.add(foto)
+            database.session.comit()
+            
+        return render_template("perfil.html", usuario=corrent_user, form=form_foto)
     else:
         usuario = Usuario.query.get(int(id.usuario))
-        return render_template("perfil.html", usuario=usuario)
+        return render_template("perfil.html", usuario=usuario, form=None)
 
 @app.route("/logout")
 @login_required
