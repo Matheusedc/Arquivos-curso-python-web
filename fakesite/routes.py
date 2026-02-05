0456f411d8ddeb5f7ffa7811b71462e0874dba66
Matheusedc <matheuseddc@gmail.com>
2026-02-05 11:51:51 -0300

    Aula 16

--- fakesite/routes.py
+++ fakesite/routes.py
@@ -73,3 +73,10 @@ def perfil(id_usuario):
 def logout():
     logout_user()
     return redirect(url_for("homepage", usuario = usuario.username))
+
+
+@app.route("/feed")
+@login_required
+def feed():
+    fotos = Foto.query.order_by(Foto.data_criacao).all()
+    return render_tamplate("feed.html")
