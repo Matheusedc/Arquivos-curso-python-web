b1cffed0c47202618dd906abfa8c4b2f454cf0e9
Matheusedc <matheuseddc@gmail.com>
2026-02-10 12:51:05 -0300

    Update __init__.py

--- fakesite/__init__.py
+++ fakesite/__init__.py
@@ -5,13 +5,14 @@ from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
-app.config["SECRET_KEY"] = "c6d4a322f3f58fa047363041458e407c"
-app.config["UPLOAD_FOLDER"] = "static/fotos_posts"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db" # Configura o banco de dados da aplicação.
+app.config["SECRET_KEY"] = "c6d4a322f3f58fa047363041458e407c" # Define a chave secreta da aplicação.
+app.config["UPLOAD_FOLDER"] = "static/fotos_posts" # onde as fotos ficarão salvas
 
 database = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
-login_manager.login_view = "homepage"
+bcrypt = Bcrypt(app) # Inicializa o Bcrypt no app.
+login_manager = LoginManager(app) # Inicializa o gerenciador de login.
+login_manager.login_view = "homepage" # Define para onde o usuário será redirecionado se tentar acessar
+# uma rota protegida sem estar logado.
 
 from fakesite import routes
